8b39b7e61003fa51eab1ed19725a85943688db38
Sofyan Eko Sanjoyo <sofyanekosanjoyo@pu.go.id>
2022-11-19 20:43:38 +0700

    Membuat Tampilan Form Login dan Registrasi SI Login

---
+++ application/controllers/Auth.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('templates/auth_header');
+        $this->load->view('auth/login');
+        $this->load->view('templates/auth_header');
+    }
+
+    public function registrasi()
+    {
+        $this->load->view('templates/auth_header');
+        $this->load->view('auth/registrasi');
+        $this->load->view('templates/auth_header');
+    }
+}
